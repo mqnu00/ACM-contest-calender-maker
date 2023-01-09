10e2eda64ebf2db969091aaa04058efed5da3d19
mqnu00 <108623923+mqnu00@users.noreply.github.com>
2023-01-09 21:18:38 +0800

    Add files via upload

---
+++ calendar_maker.py
@@ -0,0 +1,53 @@
+from codeforces_round_getter import cf_api_test
+from icalendar import Calendar, Event
+import datetime
+from pathlib import Path
+import os
+import time
+
+
+def calender_maker(contest_method, calendar_name):
+    cal = Calendar()
+
+    cal.add('prodid', '-//test.us//iCalendar Event//EN  ')      #软件信息
+    cal.add('version', '2.0')                                   #遵循的iCalendar版本号
+    cal.add('X-WR-CALNAME', calendar_name)                      #日历名
+    cal.add('X-WR-TIMEZONE', 'UTC+8')                           #时区标记
+    cal.add('X-WR-CALDESC', calendar_name + '.created by chigua_demolu.')   #日历描述
+
+    # 获取竞赛信息
+    content = contest_method()
+    # 保存目录
+    direct = Path.cwd()
+    for each in content:
+        #开始时间
+        stime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(each['stime']))
+        stime = datetime.datetime.strptime(stime, "%Y-%m-%d %H:%M:%S")
+        #结束时间
+        etime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(each['etime']))
+        etime = datetime.datetime.strptime(etime, "%Y-%m-%d %H:%M:%S")
+        event = Event()
+        #比赛名
+        event.add('summary', each['name'])
+        #比赛描述
+        event.add('description', each['name'] + ' from:' + each['link'])
+        event.add('dtstart', stime)
+        event.add('dtend', etime)
+        #事件的唯一标识
+        event['uid'] = each['name']
+        cal.add_component(event)
+
+    try:
+        direct.mkdir(parents=True, exist_ok=False)
+    except FileExistsError:
+        print("Folder already exists")
+    else:
+        print("Folder was created")
+
+    f = open(os.path.join(direct, calendar_name + '.ics'), 'wb')
+    f.write(cal.to_ical())
+    f.close()
+
+
+if __name__ == '__main__':
+    calender_maker(cf_api_test, 'codeforces-round')
---
+++ codeforces_round_getter.py
@@ -0,0 +1,38 @@
+import requests
+import os
+import json
+import time
+
+
+def cf_api_test():
+    # return [{(str)name, (int)stime, (int)etime, (str)link}]
+    os.environ['NO_PROXY'] = 'codeforces.com'
+    url = ' https://codeforces.com/api/contest.list'
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0'
+    headers = {'User-Agent': user_agent}
+    url_get = requests.get(url, headers=headers, timeout=10)
+    url_get_par = json.loads(url_get.text)
+    pattern = "%Y-%m-%d %H:%M:%S"
+    res = []
+    n = 100
+    now = time.localtime()
+    now = time.mktime(now)
+    diff = 60 * 60 * 24 * 31
+    for i in range(1, n):
+        name = url_get_par["result"][i]["name"]
+        stime = url_get_par["result"][i]["startTimeSeconds"]
+        etime = stime + 2.5 * 60 * 60
+        link = 'https://codeforces.com'
+
+        if stime - now < 0:
+            break
+
+        res.append({'name': name, 'stime': stime, 'etime': etime, 'link': link})
+
+    return res
+
+
+
+if __name__ == '__main__':
+    for each in cf_api_test():
+        print(each)
